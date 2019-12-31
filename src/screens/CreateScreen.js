a9b5194ed1f61a4a92c5fa08f12b3b3c3f26d191
Connor Loftus <connorlof@gmail.com>
2019-12-30 19:26:18 -0500

    details screen added

---
+++ src/screens/CreateScreen.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+const CreateScreen = ({ navigation }) => {
+
+    return (
+        <View>
+            <Text>Create Screen</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    
+});
+
+export default CreateScreen;
--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,13 +1,13 @@
 import React, { useContext } from 'react';
-import { View, Text, StyleSheet, FlatList, Button } from 'react-native';
+import { View, Text, StyleSheet, FlatList, Button, TouchableOpacity } from 'react-native';
 import { Context as BlogContext } from '../context/BlogContext';
+import { Feather } from '@expo/vector-icons';
 
-const IndexScreen = () => {
-    const { state, addBlogPost } = useContext(BlogContext);
+const IndexScreen = ({ navigation }) => {
+    const { state, addBlogPost, deleteBlogPost } = useContext(BlogContext);
 
     return (
         <View>
-            <Text>Index Screen</Text>
             <Button 
                 title="Add Post" 
                 onPress={addBlogPost}
@@ -16,7 +16,16 @@ const IndexScreen = () => {
                 data={state}
                 keyExtractor={blogPost => blogPost.title}
                 renderItem={({ item }) => {
-                    return <Text>{item.title}</Text>;
+                    return (
+                        <TouchableOpacity onPress={() => navigation.navigate('Show', { id: item.id })}>
+                            <View style={styles.rowStyle}>
+                                <Text style={styles.titleStyle} >{item.title} - {item.id}</Text>
+                                <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
+                                    <Feather name="trash" style={styles.iconStyle} />
+                                </TouchableOpacity>
+                            </View>
+                        </TouchableOpacity>
+                    );
                 }}
             />
         </View>
@@ -24,7 +33,20 @@ const IndexScreen = () => {
 };
 
 const styles = StyleSheet.create({
-
+    rowStyle: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingVertical: 12,
+        paddingHorizontal: 10,
+        borderBottomWidth: 1,
+        borderColor: 'gray'
+    },
+    titleStyle: {
+        fontSize: 18
+    },
+    iconStyle: {
+        fontSize: 24
+    }
 });
 
 export default IndexScreen;
